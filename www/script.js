c1c7966aa5a869f993d8460a92ae50d291305847
Dale Harvey <dale@arandomurl.com>
2019-11-02 23:15:01 +0000

    Delete notes

--- www/script.js
+++ www/script.js
@@ -1,11 +1,13 @@
 "use script";
 
+let $ = document.querySelector.bind(document);
+
 let db = new PouchDB("notes");
 let editor = new SimpleMDE({
   autoDownloadFontAwesome: false,
   toolbar: false,
   status: false,
-  element: document.querySelector("#note textarea")
+  element: $("#note textarea")
 });
 
 let changeEvent = debounce(editorChanged, 500);
@@ -13,16 +15,8 @@ let currentNote;
 
 (async function() {
   await initUI();
-  await drawNotes();
   await hashChanged();
-  if (!currentNote) {
-    let notes = await db.allDocs({include_docs: true});
-    if (notes.total_rows) {
-      selectNote(notes.rows[0].doc);
-    } else {
-      createNote();
-    }
-  }
+  //await drawNotes();
 })();
 
 async function selectNote(note) {
@@ -41,18 +35,31 @@ async function createNote() {
   document.location = "#" + result.id;
 }
 
+async function deleteNote() {
+  await db.remove(currentNote._id, currentNote._rev);
+  currentNote = null;
+  document.location = '#';
+}
+
 async function hashChanged() {
   let id = document.location.hash.slice(1);
   try {
     let note = await db.get(id);
     selectNote(note);
-  } catch (e) {
-    console.error(e)
+    drawNotes();
+  } catch (e) { }
+  if (!currentNote) {
+    let notes = await db.allDocs({include_docs: true});
+    if (notes.total_rows) {
+      document.location = "#" + notes.rows[0].id;
+    } else {
+      createNote();
+    }
   }
 }
 
 async function searchChanged() {
-  drawNotes([search]);
+  drawNotes();
 }
 
 async function editorChanged() {
@@ -66,7 +73,7 @@ async function editorChanged() {
 async function drawNotes() {
 
   let notes = (await db.allDocs({include_docs: true})).rows;
-  let search = document.querySelector("#search").value;
+  let search = $("#search").value;
 
   if (search) {
     notes = notes.filter((value) => {
@@ -76,33 +83,34 @@ async function drawNotes() {
   }
 
   if (!notes.length) {
-    document.querySelector("#notes-list").innerHTML =
-      "Nothing matched the search";
+    $("#notes-list").innerHTML = "Nothing matched the search";
     return;
   }
 
   notes.sort((a, b) => b.doc.updated - a.doc.updated);
   let titles = notes.map(note => {
     let data = note.doc.note || "";
-    let title = data.split("\n")[0].trim() ||
-      "New Note";
-    return `<li><a href="#${note.doc._id}">${title}</a></li>`;
+    let title = data.split("\n")[0].trim() || "New Note";
+    let selected = "";
+    if (currentNote && note.doc._id == currentNote._id) {
+      selected = 'class="selected"';
+    }
+    return `<li><a ${selected} href="#${note.doc._id}">${title}</a></li>`;
   });
 
-  document.querySelector("#notes-list").innerHTML =
-    titles.join("");
+  $("#notes-list").innerHTML = titles.join("");
 }
 
 async function initUI() {
-  document.querySelector("#create-note")
-    .addEventListener("click", createNote);
+  $("#create-note").addEventListener("click", createNote);
+  $("#delete-note").addEventListener("click", deleteNote);
 
   window.addEventListener('hashchange', hashChanged, false);
 
   db.changes({since: 'now', live: true})
     .on("change", drawNotes);
 
-  document.querySelector("#search")
+  $("#search")
     .addEventListener("input", debounce(searchChanged, 500));
 }
 
