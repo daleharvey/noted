0ddb1865b1b5405062f0eebe99a4d07c52042263
Dale Harvey <dale@arandomurl.com>
2019-11-10 22:24:33 +0000

    Basic sync implementation

--- index.js
+++ index.js
@@ -1,7 +1,145 @@
-const serve = require("koa-static");
-const Koa = require("koa");
-const app = new Koa();
+"use strict";
 
-app.use(serve("www"));
+const DB_DIR = __dirname + "/data/";
+const SECRET = process.env.SECRET || "kittens";
+
+const crypto = require("crypto");
+const fs = require("fs");
+
+const express = require("express");
+const bodyParser = require('body-parser');
+
+const PouchDB = require("pouchdb").defaults({
+  prefix: DB_DIR
+});
+PouchDB.plugin(require('pouchdb-find'));
+
+const nanoid = require('nanoid');
+const nodemailer = require('nodemailer');
+
+const app = express();
+const tokens = new PouchDB("tokens");
+
+if (!fs.existsSync(DB_DIR)){
+  fs.mkdirSync(DB_DIR);
+}
+
+tokens.createIndex({
+  index: {fields: ["token"]}
+});
+
+async function sendEmail(to, url) {
+  let transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: 587,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS
+    }
+  });
+
+  let info = await transporter.sendMail({
+    from: 'Dale Harvey <dale@arandomurl.com>',
+    to,
+    subject: "Sync your Noted account",
+    text: url
+  });
+
+  console.log("Message sent: %s", info.messageId);
+}
+
+async function signIn(email) {
+  let dbName = crypto.createHash('md5')
+    .update(email).digest('hex');
+  // Default document, if there is an existing one then
+  // the existing passphrase will be used.
+  let doc = {
+    _id: dbName,
+    email,
+    passphrase: nanoid()
+  };
+  try {
+    doc = await tokens.get(dbName);
+  } catch (e) { }
+  // Create a new one time use token
+  doc.token = nanoid();
+
+  await tokens.put(doc);
+  let url = `${process.env.HOST}/?token=${doc.token}`;
+  await sendEmail(email, url);
+}
+
+app.use(bodyParser.json());
+app.use(express.static("www"));
+
+// Authentication entry point, takes an email address,
+// creates a token, stores the details and sends the
+// user an email with their token.
+app.post("/sign-in", async (req, res, next) => {
+  try {
+    await signIn(req.body.email);
+    res.status(201).json({ok: true});
+  } catch(e) {
+    res.status(500);
+  }
+});
+
+function denied(res, message = "Denied") {
+  res.status(403).json({ ok: false, message });
+}
+
+// Once the user has received their token, validate it
+// here. This currently isnt doing anything important
+// but makes sure I can change the token format at least.
+app.post("/authenticate", async (req, res, next) => {
+
+  let token = req.body.token;
+  let result = await tokens.find({selector: {token}});
+
+  if (!result.docs.length) {
+    return denied(res);
+  }
+
+  let doc = result.docs[0];
+  res.json({
+    ok: true,
+    email: doc.email,
+    database: doc._id,
+    passphrase: doc.passphrase
+  });
+});
+
+// Here we intercept all database requests and validate
+// them to ensure the user has sent the appropriate
+// x-auth-token code need to access that data.
+app.use("/db/", async (req, res, next) => {
+  // We dont need to validate requests to the database root
+  // they are used for generating replication checkpoints
+  if (req.path === "/") {
+    return next();
+  }
+
+  try {
+    let dbName = req.path.split("/")[1];
+    // Reading directly from the database on every request
+    // is probably expensive, if we need to optimise
+    // this would be an easy / quick win.
+    let result = await tokens.get(dbName);
+    let token = req.headers["x-auth-token"] || null;
+    if (token && token == result.passphrase) {
+      return next();
+    }
+    throw "Denied";
+  } catch(e) {
+    res.status(403).json({
+      ok: false,
+      message: "Denied"
+    });
+  }
+});
+
+// Ok we have passed authentication, pass along
+// requests to the database now.
+app.use('/db', require('pouchdb-express-router')(PouchDB));
 
 app.listen(process.env.PORT || 3000);
--- www/script.js
+++ www/script.js
@@ -1,28 +1,81 @@
 "use script";
 
-let $ = document.querySelector.bind(document);
+const $ = document.querySelector.bind(document);
+const db = new PouchDB("notes");
 
-let db = new PouchDB("notes");
-let editor = new SimpleMDE({
+const editor = new SimpleMDE({
   autoDownloadFontAwesome: false,
   toolbar: false,
   status: false,
   element: $("#note textarea")
 });
 
-let changeEvent = debounce(editorChanged, 500);
-let currentNote;
+const changeEvent = debounce(editorChanged, 500);
+
+let user = null;
+let currentNote = null;
 
 (async function() {
+
+  await loadUser();
+
+  const params = new URLSearchParams(window.location.search);
+  if (params.has("token")) {
+    await validate(params.get("token"));
+    return;
+  }
+
   await initUI();
   await hashChanged();
+
+  if (user) {
+    await initSync(user);
+  }
 })();
 
-async function selectNote(note) {
+async function loadUser() {
+  try {
+    user = await db.get("_local/user");
+    console.log(user);
+  } catch(e) {}
+}
+
+async function initSync(details) {
+  $("#notes footer").classList.toggle("logged-in", true);
+  $("#notes footer span").innerText = details.email;
+  let url = `${document.location.origin}/db/${details.database}`;
+  let remote = new PouchDB(url, {
+    fetch: function (url, opts) {
+      opts.headers.set('X-Auth-Token', details.passphrase);
+      return PouchDB.fetch(url, opts);
+    }
+  })
+  db.sync(remote, {live: true}).on("error", console.error);
+}
+
+async function validate(token) {
+  let result = await fetch('/authenticate', {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({token})
+  });
+  let json = await result.json();
+  if (json.ok) {
+    json._id = "_local/user";
+    await db.post(json);
+    document.location = "/";
+  } else {
+    console.error(json);
+  }
+}
+
+async function selectNote(note, focus = true) {
   editor.codemirror.off("change", changeEvent);
   currentNote = note;
   editor.value(note.note);
-  editor.codemirror.focus();
+  if (focus) {
+    editor.codemirror.focus();
+  }
   editor.codemirror.on("change", changeEvent);
   document.body.classList.remove("shownotes")
 }
@@ -36,8 +89,16 @@ async function createNote() {
   document.location = "#" + result.id;
 }
 
-async function signIn() {
+async function signIn(e) {
+  e.preventDefault();
+  let email = $("#email").value
   $("#sign-in-dialog").style.display = "none";
+  let result = await fetch('/sign-in', {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({email})
+  })
+  console.log(result);
   alert("Go check your email");
 }
 
@@ -125,18 +186,42 @@ function hide(id) {
   return function() { $(id).style.display = "none"; }
 }
 
+async function logout() {
+  let doc = await db.get("_local/user");
+  await db.remove(doc);
+  document.location = "/";
+}
+
+async function dbUpdated(change) {
+  await drawNotes();
+
+  // Dont update if we are currently focused in the editor
+  // this stops us trying to update the UI based on local
+  // updates but we should come up with something more
+  // reliable.
+  if (document.activeElement &&
+      document.activeElement.nodeName === "TEXTAREA") {
+    return;
+  }
+
+  if (change.id === currentNote._id) {
+    selectNote(change.doc, false);
+  }
+}
+
 async function initUI() {
   $("#create-note").addEventListener("click", createNote);
   $("#delete-note").addEventListener("click", deleteNote);
   $("#show-notes").addEventListener("click", toggleNotes);
   $("#cover").addEventListener("click", toggleNotes);
+  $("#logout").addEventListener("click", logout);
   $("#sign-in").addEventListener("click",
     showDialog("#sign-in-dialog"));
 
   window.addEventListener('hashchange', hashChanged, false);
 
-  db.changes({since: 'now', live: true})
-    .on("change", drawNotes);
+  db.changes({since: 'now', live: true, include_docs: true})
+    .on("change", dbUpdated);
 
   $("#search")
     .addEventListener("input", debounce(searchChanged, 500));
