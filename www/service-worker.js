2999cf787466721fde9d6c47e162043300d95fdd
Dale Harvey <dale@arandomurl.com>
2019-11-12 23:30:55 +0000

    Dont cache runtime

--- www/service-worker.js
+++ www/service-worker.js
@@ -62,19 +62,8 @@ self.addEventListener('fetch', event => {
   // Skip cross-origin requests, like those for Google Analytics.
   if (event.request.url.startsWith(self.location.origin)) {
     event.respondWith(
-      caches.match(event.request).then(cachedResponse => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
-
-        return caches.open(RUNTIME).then(cache => {
-          return fetch(event.request).then(response => {
-            // Put a copy of the response in the runtime cache.
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          });
-        });
+      caches.match(event.request).then(response => {
+        return response || fetch(event.request);
       })
     );
   }
